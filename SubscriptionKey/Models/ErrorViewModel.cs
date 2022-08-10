ac3ac622c6f55b1d960e4a5328158ec3e3851af3
Randy Pagels <rpagels@microsoft.com>
2022-08-10 12:40:21 -0400

    Initial Checkin

---
+++ SubscriptionKey/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubscriptionKey.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
